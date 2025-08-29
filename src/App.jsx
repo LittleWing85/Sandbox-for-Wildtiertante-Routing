03e35e3c8ffbe8135e0d4985011f69d3bfcfab34
littlewing <mail@alexandra-jaeger.net>
2025-08-29 18:04:42 +0200

    First Route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-function App() {
-    return (
-        <>
-            <p>App text</p>
-        </>
-    );
-}
+import {
+    createBrowserRouter,
+    RouterProvider,
+    createRoutesFromElements,
+    Route,
+} from "react-router-dom";
+import Start from "./components/start/Start";
 
-export default App;
+const router = createBrowserRouter(
+    createRoutesFromElements(<Route path="/" element={<Start />} />)
+);
+
+export default function App() {
+    return <RouterProvider router={router} />;
+}
